1bffc5a82100ac84ac71764d7041037c332dd494
ClementSedillot <clement.sedillot@gmail.com>
2022-10-17 16:27:59 +0200

    C'est dure le JS

--- js/Active.js
+++ js/Active.js
@@ -1,9 +1,24 @@
 //SELECTEURS
-const newTodo = document.querySelector("new-todo")
-const todoList = document.querySelector("todo-list")
+let inputElement = document.querySelector('input');
+let listElement = document.querySelector('ul')
 
-
+let taskList = [
+    'bonjour',
+    'au revoir',
+];
 //ECOUTEURS
 
 //FUNCTIONS
+ function populateList(){
+    taskList.forEach(function(item){
+        let newItem = document.createElement('li')
 
+        //add span for text
+        
+        let span = document.createElement('span');
+        span.innerHTML = item;
+        newItem.appendChild(span)
+    })
+ }
+
+ populateList()
