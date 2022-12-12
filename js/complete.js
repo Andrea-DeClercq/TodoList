94a71f33e5122f8225f62b81e0ebc4ce8c1e96e0
AndreaDC <andrea.dclercq@gmail.com>
2022-12-12 09:57:10 +0100

    essai de changement css en cas de check

--- js/complete.js
+++ js/complete.js
@@ -4,10 +4,16 @@
 console.log('completed');
 
 function ifChecked(){
-    if(document.getElementById('completed').checked == true){
+    if(document.getElementById('completed').checked = true){
         console.log('ok');
-    } else {
+        document.getElementById('completed').style.color = "blue";
+    } else if(document.getElementById('completed').checked = false)  {
         console.log('not ok');
     }
+}
 
+function test(){
+    if(document.getElementById('completed1').checked = true){
+        document.getElementById('top').style.background = "blue";
+    }
 }
